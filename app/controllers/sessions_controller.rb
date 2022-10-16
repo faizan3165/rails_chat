287e7819cd97941c5a2e8358e156e4727d712213
faizan3165 <faizanmehmood3165@gmail.com>
2022-10-16 21:15:19 +0500

    add chatroom and session scaffold

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,30 @@
+class SessionsController < ApplicationController
+
+  # GET /sessions or /sessions.json
+  def index
+  end
+
+  # GET /sessions/1 or /sessions/1.json
+  # def show
+  # end
+
+  # # GET /sessions/new
+  # def new
+  # end
+
+  # # GET /sessions/1/edit
+  # def edit
+  # end
+
+  # # POST /sessions or /sessions.json
+  # def create
+  # end
+
+  # # PATCH/PUT /sessions/1 or /sessions/1.json
+  # def update
+  # end
+
+  # # DELETE /sessions/1 or /sessions/1.json
+  # def destroy
+  # end
+end
